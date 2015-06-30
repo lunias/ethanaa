642ac1dd38fbc5718eb76527d23301a57cbe70fd
Ethan Anderson <ethanaa@gmail.com>
2015-06-30 14:00:39 -0500

    more avatars and style changes

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -15,7 +15,7 @@ angular.module('ethanaaApp')
             var ageDiff = Date.now() - birthday.getTime();
             var ageDate = new Date(ageDiff);            
             
-            return Math.abs(ageDate.getUTCFullYear() - 1969);        	
+            return Math.abs(ageDate.getUTCFullYear() - 1970);        	
         }
         
 		// jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -23,15 +23,19 @@ angular.module('ethanaaApp')
 			var $anchor = $(this);
 			$('html, body').stop().animate({
 				scrollTop : $($anchor.attr('href')).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
-			
-			if ($anchor.attr('href') === '#resume') {
-				resizeFn(700);	
-			}
+			}, 1200, 'easeInOutExpo');
+			event.preventDefault();			
 		});
 
-		var callback = function() {
+		$('#navbar').on('activate.bs.scrollspy', function () {
+			var activeSection = $(".nav li.active > a").attr('href');
+			if (activeSection === '#resume') {
+				resizeFn(200);
+			}
+		});		
+		
+		var callback = function() {	
+			
 			$('.item-skills').each(
 					function() {
 						var newWidth = $(this).parent().width()
@@ -41,12 +45,14 @@ angular.module('ethanaaApp')
 							width : newWidth,
 						}, 1000);
 					});
+			
 			$('.icons-red').each(function() {
 				var height = $(this).height();
 				$(this).animate({
 					height : 14,
 				}, 2000);
 			});
+			
 		};
 		callback();
 		
